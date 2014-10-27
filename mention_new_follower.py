a36f7d046b733e8c126be1ed0371f9a9e6a68d3f
Candice McCollough <candice.mccollough@careerbuilder.com>
2014-10-27 18:12:39 -0400

    add fixtures

--- mention_new_follower.py
+++ mention_new_follower.py
@@ -1,8 +1,16 @@
 from auth import API
 import tweepy
 import datetime
+from search import Search
 
 new_api = API()
 api = new_api.authenticate()
 
-api.
+
+
+followers = api.followers()
+#grab most recent follower
+saved_follower = open("./fixtures/most_recent.txt","r+")
+if saved_follower == followers[0].screen_name:
+    
+most_recent=followers[0].
