f1e730a8f1b5f07bba08ab89d35e8530f9dd8b5d
Candice McCollough <candice.mccollough@careerbuilder.com>
2014-10-27 18:52:03 -0400

    first draft of reply to new follower

--- mention_new_follower.py
+++ mention_new_follower.py
@@ -1,16 +1,19 @@
 from auth import API
-import tweepy
-import datetime
+import tweepy, json, random
 from search import Search
 
 new_api = API()
 api = new_api.authenticate()
 
-
-
 followers = api.followers()
 #grab most recent follower
 saved_follower = open("./fixtures/most_recent.txt","r+")
-if saved_follower == followers[0].screen_name:
+if saved_follower.read() != followers[0].user.screen_name:
+    fixtures_thx4follow = open("./fixtures/msgs_to_followers.txt", "r")
+    lines = open("./fixtures/msgs_to_followers.txt").read().splitlines()
+    mention = "@" + str(followers[0].user.screen_name)
+    api.update_status(mention + random.choice(lines)) 
     
-most_recent=followers[0].
+most_recent=followers[0]
+
+                      
